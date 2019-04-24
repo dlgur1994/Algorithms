fb50de10e7f2115536852171c0683f16b548d95c
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2019-04-24 23:11:01 +0900

    Create SubmutipleOfN_1933.java

---
+++ samsung/SubmutipleOfN_1933.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SubmutipleOfN_1933 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		
+		num = sc.nextInt();
+		for(int i=1; i<=num ; i++) {
+			if(num%i == 0)
+				System.out.print(i + " ");
+		}
+	}
+}
