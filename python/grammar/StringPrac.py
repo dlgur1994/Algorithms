a85c06f4d896a4b4284d344ae213d0bf1745fdeb
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2019-07-11 10:13:46 +0900

    Update StringPrac.py

--- python/grammar/StringPrac.py
+++ python/grammar/StringPrac.py
@@ -6,11 +6,14 @@ print(str[0])
 print(str[1])
 print(str[-1])
 print(str[-2])
-print (len(str))
+print(len(str))
 
 print(strcopy, str==strcopy)
 print(str*2)
 print(str + " " + "I'm good")
 
-print ("How" in str)
-print ("Hello" not in str)
+print("How" in str)
+print("Hello" not in str)
+
+print(str[1:len(str):2])
+print(str[5::-2])
