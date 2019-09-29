56f29306d26822ce75cb2a5ca72ef52135ffadc0
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2019-09-29 17:56:20 -0400

    Update 1931_ArrangeMeetingRooms.py

--- BaekJoon/1931_ArrangeMeetingRooms.py
+++ BaekJoon/1931_ArrangeMeetingRooms.py
@@ -1,13 +1,19 @@
-numOfMeetings = int(input())
-start = []
-end = []
+def check(meeting):
+    count = 0
+    baseTime = 0
+    for time in meeting:
+        if(time[1]>=baseTime):
+            count += 1
+            baseTime = time[0]
+    return count
 
-for i in range(0,numOfMeetings):
-    a,b = map(int,input().split())
-    start.append(a)
-    end.append(b)
+if __name__ == "__main__":
+    numOfMeetings = int(input())
+    meeting = []
 
-nowMin = min(end)
-id = end.index(min(end))
-end[id] = -1
+    for i in range(0,numOfMeetings):
+        start,end = map(int,input().split())
+        meeting.append((end,start))
 
+    meeting = sorted(meeting)
+    print(check(meeting))
