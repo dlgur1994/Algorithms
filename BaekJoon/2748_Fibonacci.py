ce6cf1462de3e77a01dd6525ac49c30f568b433f
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2019-10-19 13:35:03 -0400

    Update 2748_Fibonacci.py

--- BaekJoon/2748_Fibonacci.py
+++ BaekJoon/2748_Fibonacci.py
@@ -1,17 +1,12 @@
 import sys
 
-def fiv(numb):
+def fib(numb):
     arr = [0]*numb
-    if(numb==1):
-        return 0
-    elif(numb==2):
-        return 1
-    else:
-        arr[1] = 1
-        for i in range(2,numb):
-            arr[i] = arr[i-1]+arr[i-2]
-        return arr[numb-1]
+    arr[1] = 1
+    for i in range(2,numb):
+        arr[i] = arr[i-1]+arr[i-2]
+    return arr[numb-1]
 
 num = int(sys.stdin.readline())
-result = fiv(num+1)
+result = fib(num+1)
 print(result)
