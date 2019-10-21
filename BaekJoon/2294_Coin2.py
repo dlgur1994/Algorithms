5d70258c97b275a59270dafcbda3807bfff16688
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2019-10-20 22:13:42 -0400

    Update 2294_Coin2.py

--- BaekJoon/2294_Coin2.py
+++ BaekJoon/2294_Coin2.py
@@ -1,18 +1,26 @@
 import sys
 
-kind,total = map(int,sys.stdin.readline().split())
-worth = [0]*kind
-for i in range(0,kind):
+coin,total = map(int,sys.stdin.readline().split())
+worth = [0]*(coin+1)
+for i in range(1,coin+1):
     worth[i] = int(sys.stdin.readline())
+arr = [[sys.maxsize for col in range(total+1)] for row in range(coin+1)]
+arr[0][0] = 0
+for i in range(1,total+1):
+    arr[0][i]=sys.maxsize
+for i in range(1,coin+1):
+    arr[i][0] = 0
 
-count = 0
-while True:
-    if total<0:
-        break
-    for i in range(kind,0):
-        if worth[i]<total:
-            total = total-worth[i]
-            count = count+1
-            break
+for i in range(1,coin+1):
+    for j in range(1,total+1):
+        if(worth[i]>j):
+            arr[i][j] = arr[i-1][j]
+        elif(arr[i-1][j]>arr[i][j-worth[i]]+1):
+            arr[i][j] = arr[i][j-worth[i]]+1
+        else:
+            arr[i][j] = arr[i-1][j]
 
-print(count)
+if(arr[coin][total]==sys.maxsize):
+    print(-1)
+else:
+    print(arr[coin][total])
