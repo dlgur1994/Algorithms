3bfaefef207e6c7a3e10a2cfe9d08b72371cece6
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2019-10-28 23:11:43 -0400

    Update 1260_DFSAndBFS.py

--- BaekJoon/1260_DFSAndBFS.py
+++ BaekJoon/1260_DFSAndBFS.py
@@ -2,12 +2,21 @@ import sys
 
 node,line,start = tuple(map(int,sys.stdin.readline().split()))
 graph = dict()
-temp = list()
+temp1 = list()
+temp2 = list()
 for i in range(line):
     st,en = tuple(map(int,sys.stdin.readline().split()))
-    temp.append(en)
+    temp1.append(st)
+    temp2.append(en)
     if st not in graph:
-        graph[st] = temp
-    else:
-        graph[st] = graph[st] + temp
-    temp = []
+        graph[st] = temp2
+    elif st in graph:
+        graph[st] = graph[st] + temp2
+    if en not in graph:
+        graph[en] = temp1
+    elif en in graph:
+        graph[en] = graph[en] + temp1
+    temp1 = []
+    temp2 = []
+
+print(graph)
