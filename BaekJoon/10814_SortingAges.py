dba9486a33576f0605cd493452dd8b87dcbb5ad9
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2019-11-16 18:08:41 -0500

    Update 10814_SortingAges.py

--- BaekJoon/10814_SortingAges.py
+++ BaekJoon/10814_SortingAges.py
@@ -6,13 +6,5 @@ for _ in range(num):
     age,name = sys.stdin.readline().split()
     peoDict[name] = age
 
-valueList = sorted(peoDict.values())
-nameList = list()
-for i in range(num):
-    for name,age in peoDict.items():
-        if age == valueList[i]:
-            nameList.append(name)
-            peoDict[name] = -1
-
-for i in range(num):
-    print(valueList[i],nameList[i])
+for name,age in sorted(peoDict.items(), key=lambda x: x[1]):
+    print (age,name)
