c1afae2109a4ac0a5bbad087e2ec9528e9e0edca
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2019-11-17 00:39:49 -0500

    Update 10989_Sorting3.py

--- BaekJoon/10989_Sorting3.py
+++ BaekJoon/10989_Sorting3.py
@@ -4,12 +4,12 @@ read = sys.stdin.readline
 N = int(read())
 numList = [0]*10001
 for i in range(N):
-    numList[int(read())] += 1
+    numList[int(sys.stdin.readline())] += 1
 
 for i in range(10001):
     if numList[i] > 0:
-        for j in range(numList[i]):
-            print(i)
+        print('{}\n'.format(i) * numList[i], end='')
 
 #'read = sys.stdin.readline' makes the run time short
 #'sort' function is slow
+#'{}'.format(value) --> {} is substituted by 'value'
