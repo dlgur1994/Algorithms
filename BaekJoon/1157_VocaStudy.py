53cef364484eb529f1c0f902e966be6c063cf977
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2019-11-19 11:47:06 -0500

    Update 1157_VocaStudy.py

--- BaekJoon/1157_VocaStudy.py
+++ BaekJoon/1157_VocaStudy.py
@@ -1,3 +1,23 @@
+#best version
+'''
+import sys
+
+voca = sys.stdin.readline().rstrip().upper()
+vocaList = list(set(voca))
+countList = []
+
+for e in vocaList:
+    countList.append(voca.count(e))
+if countList.count(max(countList)) == 1:
+    print(vocaList[countList.index(max(countList))])
+else:
+    print('?')
+'''
+#When there's only one input, it is better not to use 'read=sys.stdin.readline'
+
+
+#my first version
+'''
 import sys
 read = sys.stdin.readline().rstrip
 
@@ -19,7 +39,7 @@ if countList[0][1]>countList[1][1]:
     print(countList[0][0])
 else:
     print('?')
-
+'''
     
 #longer version
 '''
@@ -58,20 +78,3 @@ else:
     print('?')
 '''
 #it takes shorter time
-
-#best versiont
-'''
-import sys
-
-voca = sys.stdin.readline().rstrip().upper()
-vocaList = list(set(voca))
-countList = []
-
-for e in vocaList:
-    countList.append(voca.count(e))
-if countList.count(max(countList)) == 1:
-    print(vocaList[countList.index(max(countList))])
-else:
-    print('?')
-'''
-#When there's only one input, it is better not to use 'read=sys.stdin.readline'
