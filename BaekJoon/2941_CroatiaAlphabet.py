4ce024fa29e84a97251956411fe1bc909255f489
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2019-11-18 23:55:00 -0500

    Update 2941_CroatiaAlphabet.py

--- BaekJoon/2941_CroatiaAlphabet.py
+++ BaekJoon/2941_CroatiaAlphabet.py
@@ -15,3 +15,19 @@ for i in range(len(sen)-1):
     count += 1
 
 print(count)
+
+
+#short version
+'''
+import sys
+read = sys.stdin.readline
+voca = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+
+sen = read()
+for e in voca:
+    sen = sen.replace(e,'!')
+print(len(sen)-1)
+'''
+
+
+#str.replace('old','new') --> change 'old' in str to 'new'
