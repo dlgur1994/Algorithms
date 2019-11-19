42ee134252b641d5250b8f4ef66e96c717419eaa
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2019-11-19 11:01:54 -0500

    Update 1157_VocaStudy.py

--- BaekJoon/1157_VocaStudy.py
+++ BaekJoon/1157_VocaStudy.py
@@ -1,14 +1,21 @@
- import sys
+import sys
 read = sys.stdin.readline().rstrip
 
 voca = read()
 count = {}
 
 voca = voca.upper()
+if len(voca)==1:
+    print(voca)
+    exit()
 for e in voca:
     if e not in count:
         count[e] = 1
     else:
         count[e] += 1
 
-
+countList = sorted(count.items(), key=lambda x:x[1], reverse=True)
+if countList[0][1]>countList[1][1]:
+    print(countList[0][0])
+else:
+    print('?')
