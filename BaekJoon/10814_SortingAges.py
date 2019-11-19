adddf561d5b40adff47cfe6936e5d67a8d272c8b
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2019-11-19 10:52:58 -0500

    Update 10814_SortingAges.py

--- BaekJoon/10814_SortingAges.py
+++ BaekJoon/10814_SortingAges.py
@@ -1,12 +1,12 @@
 import sys
 
 num = int(sys.stdin.readline())
-peoDict = []
+peoList = []
 for _ in range(num):
     age,name = sys.stdin.readline().split()
-    peoDict.append((age,name))
+    peoList.append((age,name))
 
-for age,name in sorted(peoDict, key = lambda x:int(x[0])):
+for age,name in sorted(peoList, key = lambda x:int(x[0])):
     print(age, name)
     
 #have to make 'age' int
