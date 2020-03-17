8c8b27aaf29d0e1a0edf9b327184133ec41676d8
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2020-03-18 00:42:55 +0900

    Create 1313 Decompress Run-Length Encoded List.py

---
+++ LeetCode/1313 Decompress Run-Length Encoded List.py
@@ -0,0 +1,14 @@
+import sys
+read = sys.stdin.readline
+
+class Solution:
+    def decompressRLElist(self, nums):
+        result = []
+        for i in range(0,len(nums)//2):
+            result += [nums[2*i+1]] * nums[2*i] 
+        return result
+
+
+numList = list(map(int,read().split(',')))
+mod = Solution()
+print(mod.decompressRLElist(numList))
