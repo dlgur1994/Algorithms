57425944a46ed6478410e7e0f099342a6d8fe0bd
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2020-07-13 09:01:10 +0900

    Update 1436 Destination City.py

--- LeetCode/1436 Destination City.py
+++ LeetCode/1436 Destination City.py
@@ -5,17 +5,22 @@ read = sys.stdin.readline
 
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
+        intermList = []
         cityList = []
-        for e in paths:
-            #cityList.append(e.split(','))
-            print(e)
+
+        for i in range(0,int((len(paths)-1)/2)):
+            intermList.append(paths[2*i+1])
+
+        for i in range(0,len(intermList)):
+            if i%2 == 1:
+                for
+
+
         return cityList
 
-#[["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
-
-
-input = read().rstrip()
-#list(read().rstrip().split(','))
+input = list(read().rstrip().split('"'))
 mod = Solution()
 result = mod.destCity(input)
 print(result)
+
+#[["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
