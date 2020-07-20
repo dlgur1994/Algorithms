0f33ba94b12c7542a3b3eceb188542104267a98c
Brian Hyuk Lee <38805855+dlgur1994@users.noreply.github.com>
2020-07-20 22:26:49 +0900

    Update 1436 Destination City.py

--- LeetCode/1436 Destination City.py
+++ LeetCode/1436 Destination City.py
@@ -5,25 +5,20 @@ read = sys.stdin.readline
 
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        startList = []
-        destList = []
-        cityList = []
+        startList = [path[0] for path in paths]
+        destList = [path[1] for path in paths]
+         
+        for e in startList:
+            if e in destList:
+                destList.remove(e)
+        
+        return destList[0]
 
-        #separate start points and destinations
-        for i in range(0,int((len(paths))/4)):
-            startList.append(paths[4*i+1])
-            destList.append(paths[4*i+3])
-
-        for i in range(0,len(startList)):
-            if startList[i] not in destList:
-                cityList.append(startList[i])
-                cityList.append(destList[i])
-
-        return cityList
-
-input = list(read().rstrip().split('"'))
+# input = list(read().rstrip().split('"'))
+input = read()
+print(input)
 mod = Solution()
 result = mod.destCity(input)
 print(result)
 
-#[["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+#[["Lima","Sao Paulo"],["London","New York"],["New York","Lima"]]
