cfb7929ffe46b8cacedf15a8749f1efe87193339
dlgur1994 <21400611@handong.edu>
2020-09-21 17:16:04 +0900

    1370 done

--- LeetCode/1370_Increasing_Decreasing_String.py
+++ LeetCode/1370_Increasing_Decreasing_String.py
@@ -1,32 +1,51 @@
 import sys
 read = sys.stdin.readline
+from collections import Counter
 
 class Solution:
     def sortString(self, s: str) -> str:
-        output = []
-        temp = sorted(list(s))
+        char_dict = Counter(s)
+        key_list = sorted(char_dict.keys())
+        output = ""
 
-        while len(temp)>0:
-            output.append(temp[0])
-            temp.remove(temp[0])
-            for e in temp:
-                if e>output[-1]:
-                    output.append(e)
-                    temp[temp.index(e)] = ''
-            temp = [e for e in temp if e]
+        while len(output) < len(s):
+            for e in key_list:
+                if char_dict[e] > 0:
+                    output += e
+                    char_dict[e] -= 1
+            for e in reversed(key_list):
+                if char_dict[e] > 0:
+                    output += e
+                    char_dict[e] -= 1
 
-            if len(temp)==0:
-                break
+        return output
 
-            output.append(temp[-1])
-            temp.remove(temp[-1])
-            for i in range(len(temp)-1,0,-1):
-                if temp[i]<output[-1]:
-                    output.append(temp[i])
-                    temp[i] = ''
-            temp = [e for e in temp if e]
-
-        return ''.join(output)
+# class Solution:
+#     def sortString(self, s: str) -> str:
+#         output = []
+#         temp = sorted(list(s))
+#
+#         while len(temp)>0:
+#             output.append(temp[0])
+#             temp.remove(temp[0])
+#             for e in temp:
+#                 if e>output[-1]:
+#                     output.append(e)
+#                     temp[temp.index(e)] = ''
+#             temp = [e for e in temp if e]
+#
+#             if len(temp)==0:
+#                 break
+#
+#             output.append(temp[-1])
+#             temp.remove(temp[-1])
+#             for i in range(len(temp)-1,0,-1):
+#                 if temp[i]<output[-1]:
+#                     output.append(temp[i])
+#                     temp[i] = ''
+#             temp = [e for e in temp if e]
+#
+#         return ''.join(output)
 
 input = read().rstrip()
 mod = Solution()
