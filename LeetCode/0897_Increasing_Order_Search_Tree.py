f9111bbac749e9735a0ad53395f1ba79bab5c473
dlgur1994 <21400611@handong.edu>
2020-09-28 17:57:03 +0900

    0897 ing

--- LeetCode/0897_Increasing_Order_Search_Tree.py
+++ LeetCode/0897_Increasing_Order_Search_Tree.py
@@ -2,24 +2,60 @@ import sys
 read = sys.stdin.readline
 
 class TreeNode:
-    def __init__(self, value=0, left=None, right=None):
-        self.val = value
-        self.left = left
-        self.right = right
+    def __init__(self,data):
+        self.data = data
+        self.left, self.right = None, None
 
 class BinaryTree:
-    def __init__(self,value):
-        self.root = TreeNode(value,none,none)
+    def __init__(self):
+        self.root = None
 
+    def insert_node(self,data):
+        new_node = TreeNode(data)
+        self.cur = self.root
+        if self.root == None:
+            self.root = new_node
+        else:
+            while True:
+                if new_node.data < self.cur.data:
+                    if self.cur.left == None:
+                        self.cur.left = new_node
+                        break
+                    else:
+                        self.cur = self.cur.left
+                else:
+                    if self.cur.right == None:
+                        self.cur.right = new_node
+                        break
+                    else:
+                        self.cur = self.cur.right
 
-    def insert_node(self,value):
+    # def print_node(self, node):
+    #     if node == None:
+    #         pass
+    #     else:
+    #         print (node.data)
+    #         self.print_node(node.left)
+    #         self.print_node(node.right)
 
-# class Solution:
-#     def increasingBST(self, root: TreeNode) -> TreeNode:
+class Solution:
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        output = []
+        if root == None:
+            pass
+        else:
+            self.increasingBST(root.left)
+            print(root.data)
+            output.append(root.data)
+            output.append('null')
+            self.increasingBST(root.right)
+        return output
 
-input = list(read().rstrip(']').lstrip('[').split(','))
-tree_node = Tree()
+input = list(read().rstrip().rstrip(']').lstrip('[').split(','))
+tree_node = BinaryTree()
 for e in input:
-    tree_node.insert_node(e)
-# mod = Solution()
-# print(mod.increasingBST(input))
+    if e == 'null':
+        continue
+    tree_node.insert_node(int(e))
+mod = Solution()
+print(mod.increasingBST(tree_node.root))
