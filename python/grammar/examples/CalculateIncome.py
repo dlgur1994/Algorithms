bcb1ef93b98cd64a554df03e314810eddd151e2c
Brian <sydbne17@gmail.com>
2020-10-04 14:41:30 +0900

    files renamed

--- python/grammar/examples/CalculateIncome.py
+++
@@ -1,3 +0,0 @@
-hour = input("How did you work? ")
-income = float(hour)*2.75
-print(income)
--- samsung/SubmutipleOfN_1933.java
+++ samsung/SubmutipleOfN_1933.java
@@ -4,7 +4,7 @@ public class SubmutipleOfN_1933 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num;
-		
+
 		num = sc.nextInt();
 		for(int i=1; i<=num ; i++) {
 			if(num%i == 0)
