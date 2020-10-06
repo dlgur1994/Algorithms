684631fca6795bae14d6b911249d5c833fe81d97
dlgur1994 <21400611@handong.edu>
2020-10-06 17:53:39 +0900

    0797 10/06 ing

--- LeetCode/0797_All_Paths_From_Source_to_Target.py
+++ LeetCode/0797_All_Paths_From_Source_to_Target.py
@@ -4,9 +4,17 @@ read = sys.stdin.readline
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        
+        def dfs(start,end):
+            route = [start]
+            option = [[start] for i in range(len(end))]
+            for i in range(len(end)):
+                option[i].append(dfs(end[i],graph[end[i]]))
+            return option
 
-
+        output = []
+        for i in range(len(graph)):
+            output.append(dfs(i,graph[i]))
+        return output
 
 input = list(read().rstrip().lstrip('[').rstrip(']').split('],['))
 for i in range(len(input)):
