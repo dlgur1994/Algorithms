6147057ba52c3c85993478835189561709a7a1a2
dlgur1994 <21400611@handong.edu>
2020-10-13 17:53:29 +0900

    10/13 0107 ing

--- LeetCode/0107_Binary_Tree_Level_Order_Traversal_II.py
+++ LeetCode/0107_Binary_Tree_Level_Order_Traversal_II.py
@@ -23,35 +23,38 @@ class BinaryTree:
 class Solution:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         def bfs(_root):
-            visited = []
-            queue = deque([_root])
+            visited = [[_root.val]]
+            queue = deque([(_root,0)])
+            height = 1
             while queue:
-                node = queue.popleft()
-                if node not in visited:
-                    visited.append(node.val)
+                temp = []
+                for h in range(2**(height-1)):
+                    node,height = queue.popleft()
                     if node.left:
-                        queue.append(node.left)
+                        queue.append((node.left,height))
+                        temp.append(node.left.val)
                     if node.right:
-                        queue.append(node.right)
+                        queue.append((node.right,height))
+                        temp.append(node.right.val)
+                visited.append(temp)
+                height += 1
+
             return visited
 
-        def groupNodes(nodes):
-            cnt = 0
-            while True:
-                temp = []
-                for _ in range(int(2**cnt)):
-                    if len(nodes)==0:
-                        output.append(temp)
-                        return
-                    temp.append(int(nodes[0]))
-                    nodes.pop(0)
-                output.append(temp)
-                cnt += 1
+        # def groupNodes(nodes):
+        #     cnt = 0
+        #     while True:
+        #         temp = []
+        #         for _ in range(int(2**cnt)):
+        #             if len(nodes)==0:
+        #                 output.append(temp)
+        #                 return
+        #             temp.append(int(nodes[0]))
+        #             nodes.pop(0)
+        #         output.append(temp)
+        #         cnt += 1
 
-        output = []
-        node_list = bfs(root)
-        groupNodes(node_list)
-        return list(reversed(output))
+        return list(reversed(bfs(root)))
 
 
 input = list(map(TreeNode,read().rstrip().lstrip('[').rstrip(']').split(',')))
