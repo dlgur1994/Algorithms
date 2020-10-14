a87f68e6154d1f1b2ac54c41c8eb44fe0c5a79c4
dlgur1994 <21400611@handong.edu>
2020-10-14 15:46:18 +0900

    0107 10/14 done

--- LeetCode/0107_Binary_Tree_Level_Order_Traversal_II.py
+++ LeetCode/0107_Binary_Tree_Level_Order_Traversal_II.py
@@ -23,38 +23,22 @@ class BinaryTree:
 class Solution:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         def bfs(_root):
-            visited = [[_root.val]]
+            visited = []
             queue = deque([(_root,0)])
-            height = 1
             while queue:
-                temp = []
-                for h in range(2**(height-1)):
-                    node,height = queue.popleft()
-                    if node.left:
-                        queue.append((node.left,height))
-                        temp.append(node.left.val)
-                    if node.right:
-                        queue.append((node.right,height))
-                        temp.append(node.right.val)
-                if temp!=[]:
-                    visited.append(temp)
-                height += 1
-
+                node,height = queue.popleft()
+                if height>=len(visited):
+                    visited.append([node.val])
+                else:
+                    visited[height].append(node.val)
+                if node.left:
+                    queue.append((node.left,height+1))
+                if node.right:
+                    queue.append((node.right,height+1))
             return visited
 
-        # def groupNodes(nodes):
-        #     cnt = 0
-        #     while True:
-        #         temp = []
-        #         for _ in range(int(2**cnt)):
-        #             if len(nodes)==0:
-        #                 output.append(temp)
-        #                 return
-        #             temp.append(int(nodes[0]))
-        #             nodes.pop(0)
-        #         output.append(temp)
-        #         cnt += 1
-
+        if root == None:
+            return []
         return list(reversed(bfs(root)))
 
 
