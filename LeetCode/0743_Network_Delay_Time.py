2aed6dcc664bc555aee32681822b4e9346d4c24b
dlgur1994 <21400611@handong.edu>
2020-10-19 17:58:18 +0900

    20/10/19 work

--- LeetCode/0743_Network_Delay_Time.py
+++ LeetCode/0743_Network_Delay_Time.py
@@ -16,7 +16,8 @@ class Solution:
             source,target,time = deque.pop(0)
             if target not in visited:
                 visited.append((K,target,time))
-        print(visited)
+            for e in times:
+
             # if time>max_time: max_time=time
             # for i in range(len(times)):
             #     if times[i][0]==target:
@@ -34,3 +35,28 @@ refined_input_list = [[int(e) for e in element.split(',')] for element in input_
 node_num,source = map(int,read().rstrip().split(','))
 mod = Solution()
 print(mod.networkDelayTime(refined_input_list,node_num,source))
+
+'''
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        visited = [K]
+        deque = ([])
+        max_time = 0
+        for i in range(len(times)):
+            if times[i][0]==K:
+                deque.append(times[i])
+
+        while deque:
+            source,target,time = deque.pop(0)
+            if time>max_time: max_time=time
+            for i in range(len(times)):
+                if times[i][0]==target:
+                    times[i][2]+=time
+                    deque.append(times[i])
+            if target not in visited:
+                visited.append(target)
+            if len(visited)==N:
+                return max_time
+
+        return -1
+'''
