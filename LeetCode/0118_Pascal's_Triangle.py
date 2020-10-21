a3c8d50915bb42993a55706b2416cfa9a93c02df
dlgur1994 <21400611@handong.edu>
2020-10-21 13:28:56 +0900

    0118 ing 20/10/21

--- LeetCode/0118_Pascal's_Triangle.py
+++ LeetCode/0118_Pascal's_Triangle.py
@@ -1,13 +1,14 @@
 import sys
 read = sys.stdin.readline
+from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         triangle = []
         for i in range(1,numRows+1):
             row = [1 for _ in range(i)]
-            for j in range(1,i):
-
+            for j in range(1,i-1):
+                row[j]=0
             triangle.append(row)
         return triangle
 
