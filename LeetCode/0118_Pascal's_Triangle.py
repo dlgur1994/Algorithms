35ca7a44251e4a5db5a75c0496a78146db4a349f
dlgur1994 <21400611@handong.edu>
2020-10-22 16:17:12 +0900

    0118 done 20/10/22

--- LeetCode/0118_Pascal's_Triangle.py
+++ LeetCode/0118_Pascal's_Triangle.py
@@ -7,11 +7,11 @@ class Solution:
         triangle = []
         for i in range(1,numRows+1):
             row = [1 for _ in range(i)]
-            for j in range(1,i-1):
-                row[j]=0
+            if i>2:
+                for j in range(1,i-1):
+                    row[j]=triangle[i-2][j-1]+triangle[i-2][j]
             triangle.append(row)
         return triangle
-
 
 input_num = int(read().rstrip())
 mod = Solution()
