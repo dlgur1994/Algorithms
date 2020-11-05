fb9ec7a3905b694fe247b4732f7bc097e8da20d2
dlgur1994 <21400611@handong.edu>
2020-11-05 17:43:15 +0900

    0206 done 20/11/05

--- LeetCode/0206_Reverse_Linked_List.py
+++ LeetCode/0206_Reverse_Linked_List.py
@@ -25,18 +25,32 @@ class LinkedList:
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        if head == None:
-            return None
-        values = []
+        cur = None
+        pre = None
         while head:
-            values.append(head.val)
-            head = head.next
-        new_head = ListNode(values[-1])
-        cur = new_head
-        for i in range(len(values)-2,-1,-1):
-            cur.next = ListNode(values[i])
-            cur = cur.next
-        return new_head
+            cur = head.next
+            head.next = pre
+            if not cur:
+                return head
+            pre = head
+            head = cur
+
+
+# class Solution:
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         if head == None:
+#             return None
+#         nodes = []
+#         while head:
+#             nodes.append(head)
+#             head = head.next
+#         new_head = nodes[-1]
+#         cur = new_head
+#         for i in range(len(nodes)-2,-1,-1):
+#             cur.next = nodes[i]
+#             cur = cur.next
+#         cur.next = None
+#         return new_head
 
 input_list = list(map(int,read().rstrip().split(',')))
 linked_list = LinkedList()
