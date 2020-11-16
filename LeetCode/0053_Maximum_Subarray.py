61f45e3a1e49eecac506b48dd885c287602b18e3
dlgur1994 <21400611@handong.edu>
2020-11-16 17:31:19 +0900

    0121 done 20/11/16

--- LeetCode/0053_Maximum_Subarray.py
+++ LeetCode/0053_Maximum_Subarray.py
@@ -4,16 +4,24 @@ read = sys.stdin.readline
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max = nums[0]
-        for i in range(len(nums)):
-            if nums[i]>0:
-                temp = 0
-                for j in range(i,len(nums)):
-                    temp += nums[j]
-                    max = temp if temp > max else max
-            else:
-                max = nums[i] if nums[i] > max else max
+        max = temp = nums[0]
+        for i in range(1,len(nums)):
+            temp = temp+nums[i] if temp>0 else nums[i]
+            max = temp if temp>max else max
         return max
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         max = nums[0]
+#         for i in range(len(nums)):
+#             if nums[i]>0:
+#                 temp = 0
+#                 for j in range(i,len(nums)):
+#                     temp += nums[j]
+#                     max = temp if temp > max else max
+#             else:
+#                 max = nums[i] if nums[i] > max else max
+#         return max
 
 input_list = list(map(int,read().rstrip().split(',')))
 mod = Solution()
