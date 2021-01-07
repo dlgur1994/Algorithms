8a97ff6c83422622ff3ce3592d032d48fec096bf
dlgur1994 <21400611@handong.edu>
2021-01-07 13:24:11 +0900

    0654 done 21/01/07

--- LeetCode/0654_Maximum_Binary_Tree.py
+++ LeetCode/0654_Maximum_Binary_Tree.py
@@ -11,19 +11,26 @@ class TreeNode:
 
 class Solution:
     def makeNodes(self, l):
-        if l == None:
+        if not l:
             return None
         else:
-            node = TreeNode(max(l))
-            id = l.index(max(l))
-            node.left = self.makeNodes(l[:id])
-            node.right = self.makeNodes(l[id+1:])
-        return node
+            node = TreeNode(max(l)) # find the largest number and make it a treenode
+            id = l.index(max(l)) # find the index of the biggest number
+            node.left = self.makeNodes(l[:id]) # the node's left
+            node.right = self.makeNodes(l[id+1:]) # the node's right
+            return node
 
     def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
         root = self.makeNodes(nums)
         return root
 
+def printNodes(node):
+    print(node.val)
+    if node.left:
+        printNodes(node.left)
+    if node.right:
+        printNodes(node.right)
+
 nums = list(map(int, read().rstrip().split(',')))
 mod = Solution()
-print(mod.constructMaximumBinaryTree(nums))
+printNodes(mod.constructMaximumBinaryTree(nums))
