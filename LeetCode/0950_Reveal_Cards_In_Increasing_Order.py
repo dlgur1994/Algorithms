8b0d19937fd1524152f701aec502c61d14884c3a
dlgur1994 <21400611@handong.edu>
2021-01-15 23:03:42 +0900

    0950 done 21/01/15

--- LeetCode/0950_Reveal_Cards_In_Increasing_Order.py
+++ LeetCode/0950_Reveal_Cards_In_Increasing_Order.py
@@ -1,21 +1,19 @@
 from typing import List
+from collections import deque
 import sys
 
 read = sys.stdin.readline
 
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        id = deque(range(len(deck)))
         output = [-1] * len(deck)
-        sort_deck = sorted(deck)
-        if len(deck)%2 == 1:
-            length1 = len(deck)//2 + 1
-        else:
-            length1 = len(deck)//2
-        length2 = len(deck)-length1
-        for i in range(length1):
-            output[2*i] = sort_deck[i]
-        for i in range(length2):
-            output[2*i+1] = sort_deck[length1+i]
+
+        # It is a way of simulating the conditions of the problem as they are.
+        for e in sorted(deck):
+            output[id.popleft()] = e
+            if id: # If the list id is present, send the first element to the back.
+                id.append(id.popleft())
         return output
 
 deck = list(map(int, read().rstrip().split(',')))
