cb8cee959576d993d3e3b44a6b16dfd48527d8be
dlgur1994 <21400611@handong.edu>
2021-01-19 00:06:55 +0900

    1669 done 21/01/19

--- LeetCode/1669_Merge_In_Between_Linked_Lists.py
+++ LeetCode/1669_Merge_In_Between_Linked_Lists.py
@@ -28,15 +28,15 @@ class LinkedList:
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
         cur = list1
-        for i in range(b):
+        for i in range(b): # Point to ptr1 and ptr2 at each front of a and b.
             if i == a-1:
                 ptr1 = cur
             cur = cur.next
         ptr2 = cur.next
-        ptr1.next = list2
-        while ptr1.next:
+        ptr1.next = list2 # Connect the following from ptr1 to list2.
+        while ptr1.next: # Go to the last node of the connected list2.
             ptr1 = ptr1.next
-        ptr1.next = ptr2
+        ptr1.next = ptr2 # Connect the rest of list1 to its most end node.
         return list1
 
 values1 = list(map(int, read().rstrip().split(',')))
