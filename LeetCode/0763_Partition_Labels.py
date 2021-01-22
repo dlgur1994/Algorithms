2195542089a07a3a7ca3c1e1a674ca0eab69bc81
dlgur1994 <21400611@handong.edu>
2021-01-22 23:46:35 +0900

    0763 ing 21/01/22

--- LeetCode/0763_Partition_Labels.py
+++ LeetCode/0763_Partition_Labels.py
@@ -5,29 +5,20 @@ read = sys.stdin.readline
 
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
-        vals = [S[0]]
-        cuts = []
-        id = 0
+        ids = []
+        start, end = 0, 1
 
-        for i in range(1,len(S)):
-            if S[i] not in S[i+1:]:
-                for e in vals:
-                    if e in S[i+1:]:
-                         break
-            elif S[i] not in vals:
-                vals.append(S[i])
-
-        # while S:
-        #     rev = S[::-1]
-        #     vals = S[id:len(S)-rev.index(S[id])]
-        #     print(vals)
-        #     id = len(S)-rev.index(S[id])
-        #     for val in vals:
-        #         if val in S[id:]:
-        #             id = len(S)-rev.index(S[val])
-        #     S = S[id:]
-
-
+        for i in range(len(S)):
+            check = end
+            for val in S[start:end]:
+                if len(S)-1-S[::].index(S[i]) > end:
+                    end = S.index(S[i])
+                    print('1', end)
+            if check == end:
+                ids.append(end)
+                end += 1
+                start = end
+        # return ids
 
 S = read().rstrip()
 mod = Solution()
