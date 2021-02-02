b9422589dd87c90f337ade6630af744558076eab
dlgur1994 <21400611@handong.edu>
2021-02-02 23:30:58 +0900

    0763 done 21/02/02

--- LeetCode/0763_Partition_Labels.py
+++ LeetCode/0763_Partition_Labels.py
@@ -1,24 +1,22 @@
 from typing import List
+from collections import defaultdict
 import sys
 
 read = sys.stdin.readline
 
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
-        ids = []
-        start, end = 0, 1
-
-        for i in range(len(S)):
-            check = end
-            for val in S[start:end]:
-                if len(S)-1-S[::].index(S[i]) > end:
-                    end = S.index(S[i])
-                    print('1', end)
-            if check == end:
-                ids.append(end)
-                end += 1
-                start = end
-        # return ids
+        last = defaultdict(int)
+        id, ptr = -1, 0
+        ans = [0]
+        for i, s in enumerate(S): # Store the last location of each element.
+            last[s] = i
+        for i, s in enumerate(S):
+            id = max(id, last[s]) # id refers to the backmost position of the elements in a bundle.
+            if id == i: # If the elements so far are no longer behind.
+                ans.append(i-ptr+1) # Append the length of the bundle to ans.
+                ptr += ans[-1] # Move ptr to the next position of the bundle.
+        return ans[1:] # The first element of ans is zero, so return the remaining element except it.
 
 S = read().rstrip()
 mod = Solution()
---
+++ LeetCode/1389_Create_Target_Array_in_the_Given_Order.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void) {
+  int num1;
+  std::cout << "num: "
+  std::cin >> num1;
+
+  return 0;
+}
+// class Solution {
+// public:
+//     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+//
+//     }
+// };
