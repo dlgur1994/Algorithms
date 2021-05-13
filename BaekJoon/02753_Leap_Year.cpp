43cef6bebacd3329e3628523a84f23552d0f0f63
dlgur1994 <21400611@handong.edu>
2021-05-13 22:52:34 +0900

    21/05/13 done

--- BaekJoon/02753_Leap_Year.cpp
+++ BaekJoon/02753_Leap_Year.cpp
@@ -7,9 +7,8 @@ int main(void) {
     cin >> year;
 
     if ((year%4==0 && year%100!=0) || year%400==0)
-        cout << 1;
+        cout << "1\n";
     else
-        cout << 0;
-    cout << "\n";
+        cout << "0\n";
     return 0;
 }
