c165425f020d070bd5e635dca89bd08402094aaa
dlgur1994 <21400611@handong.edu>
2021-05-20 22:55:08 +0900

    code edited

--- BaekJoon/02908_Sangsoo.cpp
+++ BaekJoon/02908_Sangsoo.cpp
@@ -24,39 +24,3 @@ int main(void) {
     }
     return 0;
 }
-
-// #include <iostream>
-
-// using namespace std;
-
-// int main(void) {
-//     int num1, num2, cp1, cp2, big;
-//     int result = 0;
-//     cin >> num1 >> num2;
-//     cp1 = num1;
-//     cp2 = num2;
-
-//     for (int i=0 ; i<3 ; i++) {
-//         if (cp1%10 > cp2%10) {
-//             big = num1;
-//             break;
-//         }
-//         else if (cp1%10 < cp2%10) {
-//             big = num2;
-//             break;
-//         }
-//         else {
-//             cp1 /= 10;
-//             cp2 /= 10;
-//         }
-//     }
-
-//     for (int i=0 ; i<3 ; i++) {
-//         result *= 10;
-//         result += big%10;
-//         big /= 10; 
-//     }
-
-//     cout << result << '\n';
-//     return 0;
-// }
