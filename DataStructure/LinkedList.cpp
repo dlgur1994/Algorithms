7c250e916a6f851f335ffec32924725d3bab63bd
dlgur1994 <21400611@handong.edu>
2021-07-03 01:24:17 +0900

    LinkedList done 21/07/03

--- DataStructure/LinkedList.cpp
+++ DataStructure/LinkedList.cpp
@@ -6,24 +6,30 @@ struct Node {
 };
 
 class LinkedList {
-private: 
+private:
+    Node * head = NULL;
+    Node * tail = NULL;
+    Node * cur = NULL;
+    Node * ptr = NULL;
+    Node * del_node = NULL;
 public:
-    // void addNode(Node * head, Node * tail, int val) {
-    //     Node * ptr = new Node();
-    //     ptr->val = val;
-    //     ptr->next = NULL;
-    //     if (head == NULL) {
-    //         head = ptr;
-    //     }
-    //     else {
-    //         tail->next = ptr;
-    //     }
-    //     tail = ptr; 
-    // }
+    void addNode(int val) {
+        ptr = new Node(); // 'new' keyword provides dynamic allocation like 'malloc' function in C language. And dynamically assigned objects must be pointed out using a pointer.
+        ptr->val = val; // '->' is a symbol used by a pointer to access an element.
+        ptr->next = NULL;
+        if (head == NULL) {
+            head = ptr;
+        }
+        else {
+            tail->next = ptr;
+        }
+        tail = ptr;
+    }
 
-    void searchNode(Node * cur, Node * tail, int target) {
+    void searchNode(int target) {
         int id = 0;
         bool flag = false;
+        cur = head;
         while (cur != tail->next) {
             if (cur->val == target) {
                 printf("target index: %d\n", id);
@@ -38,7 +44,27 @@ public:
         }
     }
 
-    void printNode(Node * cur, Node * tail) {
+    void deleteNode(int val) {
+        cur = head;
+        while (cur != tail->next) {
+            if (cur->val == val) {
+                del_node = cur;
+                head = cur->next;
+                delete del_node;
+                break;
+            }
+            else if (cur->next->val == val) {
+                del_node = cur->next;
+                cur->next = cur->next->next;
+                delete del_node;
+                break;
+            }
+            cur = cur->next;
+        }
+    }
+
+    void printNode() {
+        cur = head;
         while (cur != tail->next) {
             printf("%d ", cur->val);
             cur = cur->next;
@@ -49,29 +75,24 @@ public:
 };
 
 int main(void) {
-    LinkedList linked_list;
     int values[5] = {1, 2, 3, 4, 5};
-    Node * head = NULL;
-    Node * tail = NULL;
-    Node * cur = NULL;
+    LinkedList linked_list;
     
     for (int i=0 ; i<5 ; i++){
-        // linked_list.addNode(head, tail, values[i]);
-        Node * ptr = new Node();
-        ptr->val = values[i];
-        ptr->next = NULL;
-        if (head == NULL) {
-            head = ptr;
-        }
-        else {
-            tail->next = ptr;
-        }
-        tail = ptr;
+        linked_list.addNode(values[i]);
     }
 
-    linked_list.searchNode(head, tail, 5);
-    linked_list.searchNode(head, tail, 6);
-    linked_list.printNode(head, tail);
- 
+    linked_list.printNode();
+
+    linked_list.searchNode(5);
+    linked_list.searchNode(6);
+    
+    linked_list.deleteNode(1);
+    linked_list.printNode();
+    linked_list.deleteNode(3);
+    linked_list.printNode();
+    linked_list.deleteNode(5);
+    linked_list.printNode();
+    
     return 0;
 }
