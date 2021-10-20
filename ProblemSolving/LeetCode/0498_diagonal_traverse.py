6557317c7a7529db7e43d339f9c1297bcb025f53
dlgur1994 <21400611@handong.edu>
2021-10-20 23:30:38 +0900

    leetcode0498 ing 21/10/20

--- ProblemSolving/LeetCode/0498_diagonal_traverse.py
+++ ProblemSolving/LeetCode/0498_diagonal_traverse.py
@@ -4,19 +4,37 @@ from typing import List
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         result = []
-        for row in range(len(mat)):
-            col = 0
-            while row > -1:
+        row, col = 0, 0
+        dir = 'up'
+        while len(result) < len(mat)*len(mat[0]):
+            if row>-1 and row<len(mat) and col>-1 and col<len(mat[0]):
                 result.append(mat[row][col])
+            if dir == 'up':
                 row -= 1
                 col += 1
-        for col in range(1, len(mat)):
-            row = 0
-            while col < len(mat):
-                result.append(mat[row][col])
-                row -= 1
-                col += 1
+            else:
+                row += 1
+                col -= 1
+            if row == -1:
+                dir = 'up'
+            else:
+                dir = 'down'
         return result
+
+# class Solution:
+#     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+#         result = [mat[0][0]]
+#         dir = 'down'
+#         for i in range(1,len(mat)+len(mat[0])-1):
+#             for j in range(i+1):
+#                 if dir == 'down':
+#                     if j<len(mat) and i-j<len(mat[0]):
+#                         result.append(mat[j][i-j])
+#                 else:
+#                     if i-j<len(mat) and j<len(mat[0]):
+#                         result.append(mat[i-j][j])
+#             dir = 'up' if dir=='down' else 'down'
+#         return result
 
 matrix = []
 rows = list(sys.stdin.readline().rstrip().lstrip('[').rstrip(']').split('],['))
