d2eb051735720011bde6dc71bdb5df8c6e5c5f28
dlgur1994 <21400611@handong.edu>
2021-10-24 22:37:27 +0900

    leetcode1302 ing 21/10/24

---
+++ ProblemSolving/LeetCode/1302_deepest_leaves_sum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    // Node() : val(0), left(nullptr), right(nullptr) {}
+    // Node(int x) : val(x), left(nullptr), right(nullptr) {}
+    // Node(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+struct BinaryTree {
+public:
+
+};
+
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        
+    }
+};
+
+int main(void) {
+
+    return 0;
+}
