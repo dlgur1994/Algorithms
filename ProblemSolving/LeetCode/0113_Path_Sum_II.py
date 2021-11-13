f726f4fea9039b0fdbe9a2bc464fb15fc879a87e
dlgur1994 <21400611@handong.edu>
2021-11-13 16:58:28 +0900

    0113 ing 21/11/13

---
+++ ProblemSolving/LeetCode/0113_Path_Sum_II.py
@@ -0,0 +1,52 @@
+import sys
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Tree:
+    def __init__(self):
+        self.root = None
+    
+    def addNode(self, center, left, right): 
+        if self.root == None:
+            self.root = center
+            center.val = int(center.val)
+        
+        if left.val != 'null':
+            center.left = left
+            left.val = int(left.val)
+        if right.val != 'null':
+            center.right = right
+            right.val = int(right.val)
+
+    def printNode(self, node):
+        print(node.val)
+        if node.left:
+            self.printNode(node.left)
+        if node.right:
+            self.printNode(node.right)
+
+class Solution:
+    def dfs(node, sum, targetsum):
+        if sum + node.val == targetsum:
+            
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        ans = []
+        ans = dfs(root)
+        return ans
+
+nodes = list(map(TreeNode,sys.stdin.readline().rstrip().split(',')))
+targetsum = int(sys.stdin.readline())
+
+tree = Tree()
+for i in range(len(nodes)//2):
+    tree.addNode(nodes[i], nodes[2*i+1], nodes[2*i+2])
+# tree.printNode(tree.root)
+
+mod = Solution()
+print(mod.pathSum(tree.root, targetsum))
